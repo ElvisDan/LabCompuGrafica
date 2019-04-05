661da2e71085857dc24e2a62be96b6f78a70e800
ElvisDan <cazador.fenix@hotmail.com>
2019-04-05 13:28:47 -0600

    Practica 8

--- Practica8/16-IluminacionBasica/src/main.cpp
+++ Practica8/16-IluminacionBasica/src/main.cpp
@@ -263,6 +263,14 @@ void applicationLoop() {
 		glm::mat4 lightModelmatrix = glm::rotate(cubeModelMatrix, angle, glm::vec3(0.0f, 1.0f, 0.0f));
 		lightModelmatrix = glm::translate(lightModelmatrix, glm::vec3(0.0f, 0.0f, -ratio));
 
+		iluminacionShader.turnOn();//Se activa el shader
+		glUniform3f(iluminacionShader.getUniformLocation("light.ambient"), 0.3, 0.3, 0.3);//Se manda color a los cubos 
+		glUniform3f(iluminacionShader.getUniformLocation("light.diffuse"), 0.5, 0.2, 0.7);
+		glUniform3f(iluminacionShader.getUniformLocation("light.specular"), 0.8, 0.3, 0.5);
+		glUniform3fv(iluminacionShader.getUniformLocation("light.position"), 1, glm::value_ptr(glm::vec3(lightModelmatrix * glm::vec4(0.0, 0.0, 0.0, 1.0))));
+		glUniform3fv(iluminacionShader.getUniformLocation("viewPos"), 1, glm::value_ptr(camera->getPosition()));
+		iluminacionShader.turnOff();//Se desactiva el shader
+
 		sphere.setProjectionMatrix(projection);
 		sphere.setViewMatrix(view);
 		sphere.enableWireMode();
